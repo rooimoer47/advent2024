55c381e6eb84eb5a8656070621e7e892a4f6703d
Grabe <ricus.grabe@capgemini.com>
2024-12-09 13:08:46 +0100

    up to and including problem 9

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -310,6 +308,21 @@ public class Main {
         }
     }
 
+    public static void print(char [][] a) {
+        for (char[] s : a) {
+            for (char c : s) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void print(List<BigInteger> a) {
+        for (BigInteger b : a) {
+            System.out.print(b);
+        }
+    }
+
     public static int fiveA(String filename) {
         List<String> list = read(filename);
         List<String[]> rules = new ArrayList<>();
@@ -413,26 +426,448 @@ public class Main {
         return sum;
     }
 
-    public static int oneT(String filename) {
+    public static int sixA(String filename) {
+        char[][] list = readBlock(filename);
+        int sum = 0;
+        int x = 0;
+        int y = 0;
+
+        for (int row = 0; row < list.length ; row++) {
+            for (int col = 0; col < list[row].length ; col++) {
+                if (list[row][col] == '^') {
+                    list[row][col] = 'X';
+                    sum++;
+                    x = row;
+                    y = col;
+                    break;
+                }
+            }
+            if (sum > 0) {
+                break;
+            }
+        }
+
+        int[][]dirs = {{-1,0}, {0,1}, {1, 0}, {0, -1}};
+        int dir = 0;
+
+        while (list[x][y] != '#') {
+            if (list[x][y] == '.') {
+                sum++;
+                list[x][y] = 'X';
+            }
+            if (x+dirs[dir][0] < 0 || x+dirs[dir][0] > list.length || y+dirs[dir][1] < 0 || y+dirs[dir][1] > list[0].length) {
+                return sum;
+            }
+            try {
+                while (list[x + dirs[dir][0]][y + dirs[dir][1]] == '#') {
+                    dir = (dir + 1) % 4;
+                    if (x + dirs[dir][0] < 0 || x + dirs[dir][0] > list.length || y + dirs[dir][1] < 0 || y + dirs[dir][1] > list[0].length) {
+                        return sum;
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return sum;
+            }
+            x = x+dirs[dir][0];
+            y = y+dirs[dir][1];
+        }
+
+        return sum;
+    }
+
+    public static boolean createsLoop(char[][] map, int startRow, int startCol, int startDir) {
+        int rows = map.length, cols = map[0].length;
+        Set<String> visited = new HashSet<>();
+        int r = startRow, c = startCol, dir = startDir;
+        int[][]directions = {{-1,0}, {0,1}, {1, 0}, {0, -1}};
+
+        while (true) {
+            String state = r + "," + c + "," + dir;
+            if (visited.contains(state)) {
+                return true; // Loop detected
+            }
+            visited.add(state);
+
+            int nextR = r + directions[dir][0];
+            int nextC = c + directions[dir][1];
+            if (nextR < 0 || nextR >= rows || nextC < 0 || nextC >= cols) {
+                return false;
+            }
+            if (map[nextR][nextC] == '#') {
+                dir = (dir + 1) % 4;
+            } else {
+                // Move forward
+                r = nextR;
+                c = nextC;
+            }
+        }
+    }
+
+    public static int sixB(String filename) {
+        char[][] list = readBlock(filename);
+        int sum = 0;
+        int x = 0;
+        int y = 0;
+        int loops = 0;
+
+        for (int row = 0; row < list.length ; row++) {
+            for (int col = 0; col < list[row].length ; col++) {
+                if (list[row][col] == '^') {
+                    list[row][col] = 'X';
+                    sum++;
+                    x = row;
+                    y = col;
+                    break;
+                }
+            }
+            if (sum > 0) {
+                break;
+            }
+        }
+
+        int dir = 0;
+
+        for (int row = 0; row < list.length ; row++) {
+            for (int col = 0; col < list[row].length ; col++) {
+                if (list[row][col] == '.') {
+                    list[row][col] = '#';
+                    if (createsLoop(list, x, y, dir)) {
+                        loops++;
+                    }
+                    list[row][col] = '.';
+                }
+            }
+        }
+        return loops;
+    }
+
+    public static int calc(int a, int b, int op) {
+        return switch (op) {
+          case 0 -> a+b;
+          case 1 -> a*b;
+//          case 2 -> a-b;
+//          case 3 -> a/b;
+            default -> throw new IllegalStateException("Unexpected value: " + op);
+        };
+
+    }
+
+    public static boolean evaluateOperators(BigInteger[] numbers, int index, BigInteger currentValue, BigInteger target) {
+        if (index == numbers.length - 1) {
+            return currentValue.equals(target);
+        }
+
+        BigInteger nextValue = numbers[index + 1];
+
+        if (evaluateOperators(numbers, index + 1, currentValue.add(nextValue), target)) {
+            return true;
+        }
+
+        if (evaluateOperators(numbers, index + 1, currentValue.multiply(nextValue), target)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static BigInteger sevenA(String filename) {
+        List<String> list = read(filename);
+        BigInteger sum = new BigInteger("0");
+        for (String line : list) {
+            String[] parts = line.split(":");
+            BigInteger ans = new BigInteger(parts[0].trim());
+            String[] intStrings = parts[1].trim().split(" ");
+            BigInteger[] intArr = Arrays.stream(intStrings).map(BigInteger::new).toArray(BigInteger[]::new);
+            if (evaluateOperators(intArr, 0, intArr[0], ans)) {
+                sum = sum.add(new BigInteger(parts[0]));
+            }
+
+        }
+
+        return sum;
+    }
+
+    public static int sevenB(String filename) {
         List<String> list = read(filename);
 
         return 0;
     }
 
+    public static Map<Character, List<int[]>> findAntennas(char[][] list) {
+        Map<Character, List<int[]>> antennas = new HashMap<>();
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                if (list[i][j] != '.') {
+                    if (!antennas.containsKey(list[i][j])) {
+                        List<int[]> coord = new ArrayList<>();
+                        coord.add(new int[]{i, j});
+                        antennas.put(list[i][j], coord);
+                    } else {
+                        antennas.get(list[i][j]).add(new int[]{i, j});
+                    }
+                }
+            }
+        }
+        return antennas;
+    }
+
+    public static boolean checkBound(int[] c, char[][] grid) {
+        return c[0] >= 0 && c[0] < grid.length && c[1] >= 0 && c[1] < grid[0].length;
+    }
+
+    public static int eightA(String filename) {
+        char[][] list = readBlock(filename);
+        Map<Character, List<int[]>> antennas = findAntennas(list);
+        Set<String> anti = new HashSet<>();
+        for (Character freq : antennas.keySet()) {
+            List<int[]> positions = antennas.get(freq);
+            for (int i = 0; i < positions.size(); i++) {
+                for (int j = i+1; j < positions.size(); j++) {
+                    int x1 = positions.get(i)[0];
+                    int y1 = positions.get(i)[1];
+                    int x2 = positions.get(j)[0];
+                    int y2 = positions.get(j)[1];
+                    int ax1 = 2*x1-x2;
+                    int ay1 = 2*y1-y2;
+                    int ax2 = 2*x2-x1;
+                    int ay2 = 2*y2-y1;
+
+                    if ((ax1 >=0 && ax1 < list.length && ay1 >=0 && ay1 < list[0].length)) {
+                        list[ax1][ay1] = '#';
+                        String coord = ax1+","+ay1;
+                        anti.add(coord);
+                    }
+                    if ((ax2 >= 0 && ax2 < list.length && ay2 >= 0 && ay2 < list[0].length)) {
+                        list[ax2][ay2] = '#';
+                        String coord = ax2+","+ay2;
+                        anti.add(coord);
+                    }
+                }
+            }
+        }
+
+//        print(list);
+        return anti.size();
+    }
+
+    public static int eightB(String filename) {
+        char[][] list = readBlock(filename);
+        Map<Character, List<int[]>> antennas = findAntennas(list);
+        Set<String> anti = new HashSet<>();
+        for (Character freq : antennas.keySet()) {
+            List<int[]> positions = antennas.get(freq);
+            for (int i = 0; i < positions.size(); i++) {
+                for (int j = i+1; j < positions.size(); j++) {
+                    int x1 = positions.get(i)[0];
+                    int y1 = positions.get(i)[1];
+                    int x2 = positions.get(j)[0];
+                    int y2 = positions.get(j)[1];
+                    int ax1 = 2*x1-x2;
+                    int ay1 = 2*y1-y2;
+                    int ax2 = 2*x2-x1;
+                    int ay2 = 2*y2-y1;
+
+                    while (ax1 >=0 && ax1 < list.length && ay1 >=0 && ay1 < list[0].length) {
+                        list[ax1][ay1] = '#';
+                        String coord = ax1+","+ay1;
+                        anti.add(coord);
+                        ax1 = ax1+x1-x2;
+                        ay1 = ay1+y1-y2;
+                    }
+                    while (ax2 >= 0 && ax2 < list.length && ay2 >= 0 && ay2 < list[0].length) {
+                        list[ax2][ay2] = '#';
+                        String coord = ax2+","+ay2;
+                        anti.add(coord);
+                        ax2 = ax2+x2-x1;
+                        ay2 = ay2+y2-y1;
+                    }
+                    String coord = x1+","+y1;
+                    anti.add(coord);
+                    list[x1][y1] = '#';
+                    coord = x2+","+y2;
+                    anti.add(coord);
+                    list[x2][y2] = '#';
+                }
+            }
+        }
+
+//        print(list);
+        return anti.size();
+    }
+
+    public static BigInteger nineA(String filename) {
+        List<String> list = read(filename);
+        String input = list.get(0);
+        List<BigInteger> round1 = new ArrayList<>();
+        boolean data = true;
+        BigInteger id = BigInteger.ZERO;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (data) {
+                for (int j = 0; j < Integer.parseInt(c + ""); j++) {
+                    round1.add(id);
+                }
+                id = id.add(BigInteger.ONE);
+                data = !data;
+            } else {
+                for (int j = 0; j < Integer.parseInt(c + ""); j++) {
+                    round1.add(new BigInteger("-1"));
+                }
+                data = !data;
+            }
+        }
+
+        List<BigInteger> round2 = new ArrayList<>();
+        int index = round1.size()-1;
+        for (int i = 0; i <= index; i++) {
+            if (!round1.get(i).equals(new BigInteger("-1"))) {
+                round2.add(round1.get(i));
+            } else {
+                while (round1.get(index).equals(new BigInteger("-1"))) {
+                    index--;
+                }
+                round2.add(round1.get(index));
+                index--;
+            }
+        }
+
+        BigInteger sum = BigInteger.ZERO;
+        for (int i = 0; i < round2.size(); i++) {
+            BigInteger prod = BigInteger.valueOf(i).multiply(round2.get(i));
+            sum = sum.add(prod);
+        }
+        return sum;
+    }
+
+    public static BigInteger nineB(String filename) {
+        List<String> list = read(filename);
+        String input = list.get(0);
+        List<BigInteger> round1 = new ArrayList<>();
+        boolean data = true;
+        BigInteger id = BigInteger.ZERO;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (data) {
+                for (int j = 0; j < Integer.parseInt(c + ""); j++) {
+                    round1.add(id);
+                }
+                id = id.add(BigInteger.ONE);
+                data = !data;
+            } else {
+                for (int j = 0; j < Integer.parseInt(c + ""); j++) {
+                    round1.add(new BigInteger("-1"));
+                }
+                data = !data;
+            }
+        }
+
+        int typecnt = 0;
+        BigInteger type = new BigInteger("-2");
+        for (int i = round1.size()-1; i >= 0; i--) {
+            if (round1.get(i).equals(type)) {
+                typecnt++;
+            } else {
+                if (!type.equals(new BigInteger("-2"))) {
+                    int mincnt = 0;
+                    int lim = i+1;
+                    for (int j=0; j<lim; j++) {
+                        if (round1.get(j).equals(new BigInteger("-1"))) {
+                            mincnt++;
+                        } else {
+                            mincnt = 0;
+                        }
+                        if (mincnt == typecnt) {
+                            for (int k=0; k<typecnt; k++) {
+                                round1.set(j-k, type);
+                                round1.set(i+k+1, new BigInteger("-1"));
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                typecnt = 1;
+                type = round1.get(i);
+
+            }
+        }
+
+        BigInteger sum = BigInteger.ZERO;
+        for (int i = 0; i < round1.size(); i++) {
+            if (round1.get(i).equals(new BigInteger("-1"))) {continue;}
+            BigInteger prod = BigInteger.valueOf(i).multiply(round1.get(i));
+            sum = sum.add(prod);
+        }
+        return sum;
+    }
+
+    public static int tenA(String filename) {
+        List<String> list = read(filename);
+
+        return 0;
+    }
+
+    public static int tenB(String filename) {
+        List<String> list = read(filename);
+
+        return 0;
+    }
+
+//    public static int oneT(String filename) {
+//        List<String> list = read(filename);
+//
+//        return 0;
+//    }
+
     public static void main(String [] args) {
         Instant start = Instant.now();
-        System.out.println("1a: " + oneA("1a.txt"));
-        System.out.println("1b: " + oneB("1a.txt"));
-        System.out.println("2a: " + twoA("2.txt"));
-        System.out.println("1b: " + twoB("2.txt"));
-        System.out.println("3a: " + threeA("3.txt"));
-        System.out.println("3b: " + threeB("3.txt"));
-        System.out.println("4a: " + fourA("4.txt"));
-        System.out.println("4b: " + fourB("4.txt"));
-        System.out.println("5a: " + fiveA("5.txt"));
-        System.out.println("5b: " + fiveB("5.txt"));
+//        System.out.println("1a: " + oneA("1a.txt"));
+//        System.out.println("1b: " + oneB("1a.txt"));
+//        System.out.println("2a: " + twoA("2.txt"));
+//        System.out.println("1b: " + twoB("2.txt"));
+//        System.out.println("3a: " + threeA("3.txt"));
+//        System.out.println("3b: " + threeB("3.txt"));
+//        System.out.println("4a: " + fourA("4.txt"));
+//        System.out.println("4b: " + fourB("4.txt"));
+//        System.out.println("5a: " + fiveA("5.txt"));
+//        System.out.println("5b: " + fiveB("5.txt"));
+//        System.out.println("6a: " + sixA("6test.txt")); //41, 4789
+//        System.out.println("6B: " + sixB("6.txt")); //338 too low
+//        System.out.println("7a: " + sevenA("7.txt")); //33003154123
+//        System.out.println("7b: " + sevenB("7test.txt"));
+//        System.out.println("8a: " + eightA("8.txt")); //291 too low 313 too high
+//        System.out.println("8b: " + eightB("8.txt")); //1133, 1138 too low
+//        System.out.println("9a: " + nineA("9.txt")); //90095094087
+//        System.out.println("9b: " + nineB("9.txt")); //too high 6362723394113
+        System.out.println("10a: " + tenA("10test.txt"));
+//        System.out.println("10b: " + tenB("10test.txt"));
+
 
         System.out.println("-----");
         System.out.println(Duration.between(start, Instant.now()).toMillis() + "ms");
+    }
+
+    public static class Coordinate {
+        private final int x;
+        private final int y;
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Necessary for using as a HashMap key: equals and hashCode
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Coordinate that = (Coordinate) o;
+            return x == that.x && y == that.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
     }
 }
